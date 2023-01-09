3fe95fd9b900e07fc6f5b930ecae57bc8ff82948
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2023-01-10 03:02:26 +0900

    ポップアップのアニメーション

--- Assets/Scripts/UranaishiPageManager.cs
+++ Assets/Scripts/UranaishiPageManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UranaishiPageManager : MonoBehaviour
 {
     [SerializeField] Button xButton;
+    [SerializeField] RectTransform rectTransform;
     public static UranaishiPageManager i;
+    Vector3 initPos;
 
     public void Initialize()
     {
@@ -15,20 +18,23 @@ public class UranaishiPageManager : MonoBehaviour
 
     void Start()
     {
+        initPos = rectTransform.position;
         i = this;
-        gameObject.SetActive(false);
+        // gameObject.SetActive(false);
         xButton.onClick.AddListener(OnClickXButton);
     }
 
     void OnClickXButton()
     {
-        gameObject.SetActive(false);
+        // gameObject.SetActive(false);
+        rectTransform.DOMoveY(-Screen.height / 2f, 0.3f).SetEase(Ease.OutCirc);
+
     }
 
 
     public void Open()
     {
-        gameObject.SetActive(true);
-
+        // gameObject.SetActive(true);
+        rectTransform.DOMoveY(initPos.y, 0.3f).SetEase(Ease.OutCirc);
     }
 }
