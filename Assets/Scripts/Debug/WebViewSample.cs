ff48bc0312a6c90fbad0accaa604b6c2431deb9e
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2023-01-17 11:06:31 +0900

    gpm削除

--- Assets/Scripts/Debug/WebViewSample.cs
+++ Assets/Scripts/Debug/WebViewSample.cs
@@ -1,185 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Gpm.WebView;
 
 public class WebViewSample : MonoBehaviour
 {
     private void Start()
     {
-        ShowUrlFullScreen();
-    }
 
 
-    // FullScreen
-    public void ShowUrlFullScreen()
-    {
-        Debug.Log(0);
-        GpmWebView.ShowUrl(
-            "https://google.com/",
-            new GpmWebViewRequest.Configuration()
-            {
-                style = GpmWebViewStyle.FULLSCREEN,
-                orientation = GpmOrientation.LANDSCAPE,
-                isClearCookie = true,
-                isClearCache = true,
-                isNavigationBarVisible = true,
-                navigationBarColor = "#4B96E6",
-                title = "The page title.",
-                isBackButtonVisible = true,
-                isForwardButtonVisible = true,
-                supportMultipleWindows = true,
-#if UNITY_IOS
-                contentMode = GpmWebViewContentMode.MOBILE
+        WebViewObject webViewObject = (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
+        webViewObject.Init(
+            ld: (msg) => Debug.Log(string.Format("CallOnLoaded[{0}]", msg)),
+            enableWKWebView: true);
+
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        webViewObject.bitmapRefreshCycle = 1;
 #endif
-            },
-            OnCallback,
-            new List<string>()
-            {
-            "USER_ CUSTOM_SCHEME"
-            });
+        webViewObject.LoadURL("https://www.google.co.jp");
+
+        webViewObject.SetVisibility(true);
     }
 
-    // Popup default
-    public void ShowUrlPopupDefault()
-    {
-        GpmWebView.ShowUrl(
-            "https://google.com/",
-            new GpmWebViewRequest.Configuration()
-            {
-                style = GpmWebViewStyle.POPUP,
-                orientation = GpmOrientation.LANDSCAPE,
-                isClearCookie = true,
-                isClearCache = true,
-                isNavigationBarVisible = false,
-                supportMultipleWindows = true,
-#if UNITY_IOS
-                contentMode = GpmWebViewContentMode.MOBILE,
-                isMaskViewVisible = true,
-#endif
-            },
-            OnCallback,
-            new List<string>()
-            {
-            "USER_ CUSTOM_SCHEME"
-            });
-    }
 
-    // Popup custom position and size
-    public void ShowUrlPopupPositionSize()
-    {
-        GpmWebView.ShowUrl(
-            "https://google.com/",
-            new GpmWebViewRequest.Configuration()
-            {
-                style = GpmWebViewStyle.POPUP,
-                orientation = GpmOrientation.LANDSCAPE,
-                isClearCookie = true,
-                isClearCache = true,
-                isNavigationBarVisible = false,
-                position = new GpmWebViewRequest.Position
-                {
-                    hasValue = true,
-                    x = (int)(Screen.width * 0.1f),
-                    y = (int)(Screen.height * 0.1f)
-                },
-                size = new GpmWebViewRequest.Size
-                {
-                    hasValue = true,
-                    width = (int)(Screen.width * 0.8f),
-                    height = (int)(Screen.height * 0.8f)
-                },
-                supportMultipleWindows = true,
-#if UNITY_IOS
-                contentMode = GpmWebViewContentMode.MOBILE,
-                isMaskViewVisible = true,
-#endif
-            }, null, null);
-    }
 
-    // Popup custom margins
-    public void ShowUrlPopupMargins()
-    {
-        GpmWebView.ShowUrl(
-            "https://google.com/",
-            new GpmWebViewRequest.Configuration()
-            {
-                style = GpmWebViewStyle.POPUP,
-                orientation = GpmOrientation.LANDSCAPE,
-                isClearCookie = true,
-                isClearCache = true,
-                isNavigationBarVisible = false,
-                margins = new GpmWebViewRequest.Margins
-                {
-                    hasValue = true,
-                    left = (int)(Screen.width * 0.1f),
-                    top = (int)(Screen.height * 0.1f),
-                    right = (int)(Screen.width * 0.1f),
-                    bottom = (int)(Screen.height * 0.1f)
-                },
-                supportMultipleWindows = true,
-#if UNITY_IOS
-                contentMode = GpmWebViewContentMode.MOBILE,
-                isMaskViewVisible = true,
-#endif
-            }, null, null);
-    }
-
-    private void OnCallback(
-        GpmWebViewCallback.CallbackType callbackType,
-        string data,
-        GpmWebViewError error)
-    {
-        Debug.Log("OnCallback: " + callbackType);
-        switch (callbackType)
-        {
-            case GpmWebViewCallback.CallbackType.Open:
-                if (error != null)
-                {
-                    Debug.LogFormat("Fail to open WebView. Error:{0}", error);
-                }
-                break;
-            case GpmWebViewCallback.CallbackType.Close:
-                if (error != null)
-                {
-                    Debug.LogFormat("Fail to close WebView. Error:{0}", error);
-                }
-                break;
-            case GpmWebViewCallback.CallbackType.PageLoad:
-                if (string.IsNullOrEmpty(data) == false)
-                {
-                    Debug.LogFormat("Loaded Page:{0}", data);
-                }
-                break;
-            case GpmWebViewCallback.CallbackType.MultiWindowOpen:
-                Debug.Log("MultiWindowOpen");
-                break;
-            case GpmWebViewCallback.CallbackType.MultiWindowClose:
-                Debug.Log("MultiWindowClose");
-                break;
-            case GpmWebViewCallback.CallbackType.Scheme:
-                if (error == null)
-                {
-                    if (data.Equals("USER_ CUSTOM_SCHEME") == true || data.Contains("CUSTOM_SCHEME") == true)
-                    {
-                        Debug.Log(string.Format("scheme:{0}", data));
-                    }
-                }
-                else
-                {
-                    Debug.Log(string.Format("Fail to custom scheme. Error:{0}", error));
-                }
-                break;
-                break;
-            case GpmWebViewCallback.CallbackType.GoBack:
-                Debug.Log("GoBack");
-                break;
-            case GpmWebViewCallback.CallbackType.GoForward:
-                Debug.Log("GoForward");
-                break;
-            case GpmWebViewCallback.CallbackType.ExecuteJavascript:
-                Debug.LogFormat("ExecuteJavascript data : {0}, error : {1}", data, error);
-                break;
-        }
-    }
 }
