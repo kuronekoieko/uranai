3ad74e16d09528e67490afe3acccac2ce0d8936c
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2023-02-23 18:02:21 +0900

    要素プレハブ化

--- Assets/_MyAssets/Scripts/Schedule/RowElement.cs
+++ Assets/_MyAssets/Scripts/Schedule/RowElement.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
-public class RowElement : MonoBehaviour
+public class RowElement : ObjectPoolingElement
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] SelectDateTimeButton[] selectDateTimeButtons;
+    [SerializeField] TextMeshProUGUI timeText;
+    [SerializeField] LayoutElement lightBorderLE;
+    [SerializeField] LayoutElement boldBorderLE;
+
+
+    public override void OnInstantiate()
     {
-        
+        for (int i = 0; i < selectDateTimeButtons.Length; i++)
+        {
+            selectDateTimeButtons[i].OnInstantiate();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Show(DateTime dateTime, bool isLastOfTheHour)
     {
-        
+        timeText.text = dateTime.ToShortTimeString();
+
+        lightBorderLE.gameObject.SetActive(!isLastOfTheHour);
+        boldBorderLE.gameObject.SetActive(isLastOfTheHour);
+
+        for (int i = 0; i < selectDateTimeButtons.Length; i++)
+        {
+            selectDateTimeButtons[i].OnOpen();
+        }
     }
+
+
+
 }
---
+++ Assets/_MyAssets/Scripts/Schedule/SelectDateTimeButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SelectDateTimeButton : MonoBehaviour
+{
+    public void OnInstantiate()
+    {
+
+    }
+
+    public void OnOpen()
+    {
+
+    }
+
+    void OnClickSelectButton(int index)
+    {
+        Debug.Log(index);
+    }
+}
